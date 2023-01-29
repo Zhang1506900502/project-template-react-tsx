b97aa7312136b0d887de8d9af46a45a0bcab5c1a
zhangchengrui <1506900502@qq.com>
2023-01-29 17:40:15 +0800

    :zap: 增加一些配置

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const webpack = require('webpack')
+console.log('NODE_ENV', process.env.NODE_ENV)
+console.log('BASE_ENV', process.env.BASE_ENV)
 module.exports = {
     // 配置入口文件
     entry: path.join(__dirname, '../src/index.tsx'), // 入口文件
@@ -11,29 +13,74 @@ module.exports = {
         clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
         publicPath: '/' // 打包后文件的公共前缀路径
     },
-
     module: {
         rules: [{
-            test: /.(ts|tsx)$/, // 匹配.ts, tsx文件
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    // 预设执行顺序由右往左,所以先处理ts,再处理jsx
-                    presets: [
-                        '@babel/preset-react',
-                        '@babel/preset-typescript'
-                    ]
+                test: /.(ts|tsx)$/, // 匹配.ts, tsx文件
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        // 预设执行顺序由右往左,所以先处理ts,再处理jsx
+                        presets: [
+                            '@babel/preset-react',
+                            '@babel/preset-typescript'
+                        ]
+                    }
                 }
-            }
-        }]
+            },
+            {
+                test: /.(css|scss|sass)$/, //匹配 css 文件
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+            },
+            {
+                test: /.(png|jpg|jpeg|gif|svg)$/, // 匹配图片文件
+                type: "asset", // type选择asset
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 10 * 1024, // 小于10kb转base64位
+                    }
+                },
+                generator: {
+                    filename: 'static/images/[name][ext]', // 文件输出目录和命名
+                },
+            },
+
+            {
+                test: /.(woff2?|eot|ttf|otf)$/, // 匹配字体图标文件
+                type: "asset", // type选择asset
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 10 * 1024, // 小于10kb转base64位
+                    }
+                },
+                generator: {
+                    filename: 'static/fonts/[name][ext]', // 文件输出目录和命名
+                },
+            },
+            {
+                test: /.(mp4|webm|ogg|mp3|wav|flac|aac)$/, // 匹配媒体文件
+                type: "asset", // type选择asset
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 10 * 1024, // 小于10kb转base64位
+                    }
+                },
+                generator: {
+                    filename: 'static/media/[name][ext]', // 文件输出目录和命名
+                },
+            },
+        ]
     },
     resolve: {
         extensions: ['.js', '.tsx', '.ts']
     },
     plugins: [
+        // ["@babel/plugin-proposal-decorators", { "legacy": true }],
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, '../public/index.html'), // 模板取定义root节点的模板
             inject: true, // 自动注入静态资源
+        }),
+        new webpack.DefinePlugin({
+            'process.env.BASE_ENV': JSON.stringify(process.env.BASE_ENV)
         })
     ]
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './app.scss'
 
+import oneImg from './assets/images/one.png';
+import fiveImg from './assets/images/five.jpg'
 function App() {
-  return <h2>webpack5-react-ts</h2>
+  const [count, setCounts] = useState('')
+  const onChange = (e: any) => {
+    setCounts(e.target.value)
+  }
+  return (
+    <>
+      {/* <img src={oneImg} alt=""  />
+        <img src={fiveImg} alt=""  />
+        <h2>webpack5-react-ts</h2> */}
+      <h2>修改一下</h2>
+      <p>受控组件</p>
+      <input type="text" value={count} onChange={onChange} />
+      <br />
+      <p>非受控组件</p>
+      <input type="text" />
+    </>
+  )
+
 }
 export default App
